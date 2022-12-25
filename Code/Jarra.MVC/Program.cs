61328db292b85b9890cc69bde54aa9b8b1b1d48f
RomaPustyrev <roma.pustyrev@gmail.com>
2022-12-25 17:50:58 +0200

    Add MVC in project

--- Code/Jarra.MVC/Program.cs
+++ Code/Jarra.MVC/Program.cs
@@ -5,9 +5,12 @@ namespace Jarra.MVC
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddMvc();
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
